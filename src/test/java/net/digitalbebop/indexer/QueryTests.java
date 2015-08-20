bdb503e8ce7ea7a0df856538bbd728e4171f0ec2
Joseph Batchik <jd@jbox.csh.rit.edu>
2015-08-19 23:52:21 -0400

    started working on unit tests

---
+++ src/test/java/net/digitalbebop/indexer/QueryTests.java
@@ -0,0 +1,15 @@
+package net.digitalbebop.indexer;
+
+import static org.junit.Assert.*;
+import org.javafp.parsecj.State;
+import org.junit.Test;
+
+public class QueryTests {
+
+    @Test
+    public void simpleStringTest() {
+        QueryLanguage.init();
+        System.out.println("-----------\n\n\n\n");
+        assertEquals(0, 0);
+    }
+}
